a8876e5a6d1b1b8166125485019cf1ace2703bec
Alexander Komarov <meinekomarov@gmail.com>
2023-10-03 22:50:59 +0600

    Fast_Fix

--- Lesson_02/HMWRK_02/src/App.java
+++ Lesson_02/HMWRK_02/src/App.java
@@ -7,65 +7,119 @@ import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-public class App {
+// Модель (Model)
+class GameModel {
+    private List<ItemGenerator> generators;
+    private List<iGameItem> openedBoxes;
+    private int score;
+    private boolean isGameOver;
 
-    // Метод для добавления генераторов в список
-    private static void addGenerators(List<ItemGenerator> generators, ItemGenerator generator, int count) {
+    public GameModel() {
+        generators = new ArrayList<>();
+        openedBoxes = new ArrayList<>();
+        score = 0;
+        isGameOver = false;
+        // Добавление генераторов разных типов и количество их в списке
+        addGenerators(new GoldGenerator(), 5);
+        addGenerators(new GemGenerator(), 5);
+        addGenerators(new SilverGenerator(), 5);
+        // newItem
+        addGenerators(new WoodBoxGenerator(), 15);
+        addGenerators(new IronChestGenerator(), 15);
+        addGenerators(new ResetBoxGenerator(), 15);
+    }
+
+    private void addGenerators(ItemGenerator generator, int count) {
         for (int i = 0; i < count; i++) {
             generators.add(generator);
         }
     }
 
-    public static void main(String[] args) {
-        Random random = new Random();
-        int numItemsToOpen = 10; // Количество коробок в игре
-        int openedBoxes = 0; // Количество открытых коробок
+    public List<ItemGenerator> getGenerators() {
+        return generators;
+    }
 
-        List<ItemGenerator> generators = new ArrayList<>();
+    public List<iGameItem> getOpenedBoxes() {
+        return openedBoxes;
+    }
 
-        // Добавление генераторов разных типов и количество их в списке
-        addGenerators(generators, new GoldGenerator(), 5);
-        addGenerators(generators, new GemGenerator(), 5);
-        addGenerators(generators, new SilverGenerator(), 5);
-        // newItem
-        addGenerators(generators, new WoodBoxGenerator(), 15);
-        addGenerators(generators, new IronChestGenerator(), 15);
-        addGenerators(generators, new ResetBoxGenerator(), 15);
-        //
+    public int getScore() {
+        return score;
+    }
 
+    public boolean isGameOver() {
+        return isGameOver;
+    }
+
+    public void setGameOver(boolean gameOver) {
+        isGameOver = gameOver;
+    }
+    
+}
+
+// Представление (View)
+class GameView {
+    public void displayGameState(GameModel model) {
+        System.out.println("Открыто коробок: " + model.getOpenedBoxes().size() + " из 10");
+    }
+
+    public String getUserInput() {
+        System.out.println("Открыть следующую коробку? (y/n)");
         Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine().toLowerCase();
+    }
+}
 
-        while (openedBoxes < numItemsToOpen) {
+// Контроллер (Controller)
+class GameController {
+    private GameModel model;
+    private GameView view;
+    private Random random;
 
-            System.out.println("Открыто коробок: " + openedBoxes + " из 10");
+    public GameController(GameModel model, GameView view) {
+        this.model = model;
+        this.view = view;
+        this.random = new Random();
+    }
 
-            System.out.println("Открыть следующую коробку? (y/n)");
-            String input = scanner.nextLine().toLowerCase();
+    public void startGame() {
+        while (model.getOpenedBoxes().size() < 10 && !model.isGameOver()) {
+            view.displayGameState(model);
+            String input = view.getUserInput();
 
             if (input.equals("y")) {
-                int randomIndex = random.nextInt(generators.size());
-                // Случайный индекс из массива
-                ItemGenerator generator = generators.get(randomIndex);
+                int randomIndex = random.nextInt(model.getGenerators().size());
+                ItemGenerator generator = model.getGenerators().get(randomIndex);
                 iGameItem item = generator.createItem();
 
-                // Проверка, является ли предмет ResetBox (костыли и велосипеды)
                 if (item instanceof ResetBox) {
                     item.open();
                     if (((ResetBox) item).isGameOver) {
+                        model.setGameOver(true);
                         System.out.println("Вы проиграли!");
                     }
                     break;
                 } else {
                     item.open();
                 }
-                openedBoxes++; // Счетчик отрытых коробок
-            } else if (input.equals("n")) {// Выход из цикла while
+
+                model.getOpenedBoxes().add(item);
+            } else if (input.equals("n")) {
                 break;
-            } else {//Ввод чего то кроме "n" или "y"
+            } else {
                 System.out.println("\nПожалуйста, вводите 'y' или 'n'.\n");
             }
         }
 
-        System.out.println("Игра завершена. Открыто коробок: " + openedBoxes + " из " + numItemsToOpen);
+        System.out.println("Игра завершена. Открыто коробок: " + model.getOpenedBoxes().size() + " из 10");
+    }
+}
+
+public class App {
+    public static void main(String[] args) {
+        GameModel model = new GameModel();
+        GameView view = new GameView();
+        GameController controller = new GameController(model, view);
+        controller.startGame();
     }
 }
